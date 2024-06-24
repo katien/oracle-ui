3869019b4de2f88760904ce0219a8189c2e62554
Katie Noland <essentialbreads@Katies-MacBook-Pro.local>
2024-06-24 10:50:45 -0600

    configure next for mina, install deps

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  // tell webpack to use the ui's instance of Mina
+  // required if using a locally linked contracts package with its own instance of Mina
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        o1js: path.resolve(__dirname, "node_modules/o1js/dist/web/index.js"),
+      };
+    }
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
